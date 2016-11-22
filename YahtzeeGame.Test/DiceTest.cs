e14f6dc870f7b2ab1a4dcba9ac10e5573f96ae56
rassek96 <rassek96@gmail.com>
2016-11-22 12:26:02 +0100

    Getting started

---
+++ YahtzeeGame.Test/DiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace YahtzeeGame.Test
+{
+    [TestFixture]
+    class DiceTest
+    {
+        [Test]
+        public void ShouldReturnDiceValue()
+        {
+            model.Dice sut = new model.Dice();
+        }
+    }
+}
